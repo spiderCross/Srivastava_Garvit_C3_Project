2094d96e851cbcf47b41cae31414f8f3b593d9f7
GARVIT <garvit@GARVITs-MacBook-Air.local>
2022-04-03 20:10:05 +0530

    Part 3: Solution

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -1,3 +1,5 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,25 +23,27 @@ public class Restaurant {
         return currentTime.compareTo(this.openingTime) >= 0 && currentTime.compareTo(this.closingTime) <= 0;
     }
 
-    public LocalTime getCurrentTime(){ return  LocalTime.now(); }
+    public LocalTime getCurrentTime() {
+        return LocalTime.now();
+    }
 
     public List<Item> getMenu() {
         return this.menu;
     }
 
-    private Item findItemByName(String itemName){
-        for(Item item: menu) {
-            if(item.getName().equals(itemName))
+    private Item findItemByName(String itemName) {
+        for (Item item : menu) {
+            if (item.getName().equals(itemName))
                 return item;
         }
         return null;
     }
 
     public void addToMenu(String name, int price) {
-        Item newItem = new Item(name,price);
+        Item newItem = new Item(name, price);
         menu.add(newItem);
     }
-    
+
     public void removeFromMenu(String itemName) throws itemNotFoundException {
 
         Item itemToBeRemoved = findItemByName(itemName);
@@ -48,12 +52,13 @@ public class Restaurant {
 
         menu.remove(itemToBeRemoved);
     }
-    public void displayDetails(){
-        System.out.println("Restaurant:"+ name + "\n"
-                +"Location:"+ location + "\n"
-                +"Opening time:"+ openingTime +"\n"
-                +"Closing time:"+ closingTime +"\n"
-                +"Menu:"+"\n"+getMenu());
+
+    public void displayDetails() {
+        System.out.println("Restaurant:" + name + "\n"
+                + "Location:" + location + "\n"
+                + "Opening time:" + openingTime + "\n"
+                + "Closing time:" + closingTime + "\n"
+                + "Menu:" + "\n" + getMenu());
 
     }
 
@@ -61,4 +66,11 @@ public class Restaurant {
         return name;
     }
 
+    public int getOrderTotal(@NotNull List<String> listOfItemNames) {
+        int totalPrice = 0;
+        for (String itemName : listOfItemNames) {
+            totalPrice += findItemByName(itemName).getPrice();
+        }
+        return totalPrice;
+    }
 }
